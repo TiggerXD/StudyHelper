aedbadc400bb7e4c0fa02988cdf09470cae6c27f
Tiger <tarithomsuwan@gmail.com>
2026-08-03 22:29:31 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+from database.database import login
+
+st.set_page_config(
+    page_title="Study Helper",
+    layout="centered"
+)
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+
+    st.title("Study Helper")
+
+    st.markdown("### Login")
+
+    student_id = st.text_input(
+        "Student ID"
+    )
+
+    if st.button("Login"):
+
+        user = login(student_id)
+
+        if user:
+
+            st.session_state.logged_in = True
+
+            st.session_state.student_id = user[0]
+            st.session_state.name = user[1]
+            st.session_state.role = user[2]
+
+            st.success("Login Successful!")
+
+            st.rerun()
+
+        else:
+
+            st.error("Student ID not found.")
+
+else:
+
+    st.success(f"Welcome {st.session_state.name}")
+
+    st.write("Role:", st.session_state.role)
+
+    if st.button("Logout"):
+
+        st.session_state.clear()
+
+        st.rerun()
---
+++ database/database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+DB_NAME = "database/users.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME, check_same_thread=False)
+
+
+def initialize_database():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (student_id TEXT PRIMARY KEY, name TEXT, role TEXT)")
+
+    conn.commit()
+    conn.close()
+
+def add_user(student_id, name, role="student"):
+
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT OR REPLACE INTO users VALUES (?, ?, ?)", (student_id, name, role))
+
+    conn.commit()
+    conn.close()
